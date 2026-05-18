d05b21b3f9df1b09f2d0abd7189f666fa0b7cfad
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2026-05-17 23:34:18 -0300

    estrutura de pastas do projeto configurações de dependencias

---
+++ smart-command/src/main/java/com/luizsoftware/smart_command/SmartCommandApplication.java
@@ -0,0 +1,13 @@
+package com.luizsoftware.smart_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCommandApplication.class, args);
+	}
+
+}
---
+++ smart-command/src/test/java/com/luizsoftware/smart_command/SmartCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizsoftware.smart_command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
